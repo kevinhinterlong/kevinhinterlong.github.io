5b11e1e380d2275f5f3bbb87ae26f60143bdc024
Kevin Hinterlong <kevinhinterlong@gmail.com>
2018-01-11 16:05:35 -0600

    Fix katex rendering
    It now manually iterates over each element instead of using jquery

--- assets/js/katex_render.js
+++ assets/js/katex_render.js
@@ -1,15 +1,26 @@
-$("script[type='math/tex']").replaceWith(
-  function(){
-    var tex = $(this).text();
-    return "<span class=\"inline-equation\">" + 
-           katex.renderToString(tex) +
-           "</span>";
-});
+var elements = document.getElementsByTagName('script')
 
-$("script[type='math/tex; mode=display']").replaceWith(
-  function(){
-    var tex = $(this).text();
-    return "<div class=\"equation\">" + 
-           katex.renderToString("\\displaystyle "+tex) +
-           "</div>";
+Array.prototype.forEach.call(elements, function(element) {
+  if (element.type.indexOf('math/tex') != -1) {
+     // Extract math markdown
+     var textToRender = element.innerText || element.textContent;
+
+     // Kramdown and KaTeX workaround
+     // https://github.com/rohanchandra/type-theme/issues/47
+     textToRender = textToRender.replace(/%.*/g, '');
+
+     // Create span for KaTeX
+     var katexElement = document.createElement('span');
+
+     // Support inline and display math
+     if (element.type.indexOf('mode=display') != -1){
+       katexElement.className += "math-display";
+       textToRender = '\\displaystyle {' + textToRender + '}';
+     } else {
+       katexElement.className += "math-inline";
+     }
+
+     katex.render(textToRender, katexElement);
+     element.parentNode.insertBefore(katexElement, element);
+  }
 });
